e8d5b1c0f34d50e38339e0e5427ea313aa0e8301
Kelsey Alderman <kelseyalderman@gmail.com>
2022-08-19 06:30:11 -0700

    created employee class

--- Program.cs
+++ Program.cs
@@ -28,6 +28,8 @@ namespace CatWorx.BadgeMaker
             {
                 break;
             }
+            // Create a new Employee instance
+            Employee currentEmployee = new Employee();
             employees.Add(input);
         }
         return employees;
